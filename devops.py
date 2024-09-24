fc4743bf000e52086658afe54c74edd09576ba5f
aazabika <aazab22@mail.ru>
2024-09-24 18:41:28 +0500

    first commit

---
+++ devops.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Bye World")
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+print("Hello World")
